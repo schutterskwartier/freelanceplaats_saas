1b6efcf2bf45e24d097e58bb9afcbf990ff81910
Dennis van Rijswijk <dennis@schutterskwartier.nl>
2015-09-19 19:14:57 +0000

    Added validation & create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,22 @@ class ContactsController < ApplicationController
         @contact = Contact.new
     end
     
+    # in het formulier staat action="/contacts" 
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Message is send."
+            redirect_to new_contact_path
+        else
+            flash[:danger] = "Error all fields are mandatory!"
+            redirect_to new_contact_path
+        end
     end
+    
+    private #als veiligheidsmaatregel moet dit
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
+    
 end

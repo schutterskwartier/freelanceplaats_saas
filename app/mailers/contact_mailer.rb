1a18c48173588b9eb1c11f62b3fe897ed9d758b5
Dennis van Rijswijk <dennis@schutterskwartier.nl>
2015-09-21 19:36:59 +0000

    Fixed typo in contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::base
     default to: 'dennis@schutterskwartier.nl'
 
-    def cantact_email(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
